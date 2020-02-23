e9fe1434b3a51c3429eb0c8914c656ba7706fad3
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2020-02-23 02:43:07 -0500

    remove extra files and set up core files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
+/*
+GROUP 4: ARANSA GARCIA, JOSEPH P. PASAOA, KATHY PUMA, AND SERGIO SALAMA
+Client APP Main | Greenlist Registry (a full-stack sustainable material forum app)
+*/
+
+
+/* IMPORTS */
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
+
+/* MAIN */
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello, world</h1>
+      Welcome to the Greenlist Registry.
     </div>
   );
 }
