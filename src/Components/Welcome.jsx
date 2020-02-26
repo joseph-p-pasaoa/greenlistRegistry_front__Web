cd88e6794fb5d46c7d189f048a714c08295c6b9d
aransagarcia <aransagarcia@pursuit.org>
2020-02-25 19:48:54 -0500

    login button functioning

--- src/Components/Welcome.jsx
+++ src/Components/Welcome.jsx
@@ -1,19 +1,71 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
-const Welcome = () => {
-  return (
-    <div className='container'>
-      <h1>Welcome</h1>
-      <br/>
-      <input type='text' placeholder='username'></input>
-      <input type='text' placeholder='password'></input>
-      <button>Log In</button>
-      <Link to='/register'>
-      <button>Sign Up</button>
-      </Link>
-    </div>
-  )
+class Welcome extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      username: "",
+      password: "",
+      id: ""
+    };
+  }
+
+  handleInput = event => {
+    this.setState({
+      [event.target.name]: event.target.value,
+      [event.target.name]: event.target.value
+    });
+  };
+
+  submitForm = async event => {
+    event.preventDefault();
+    let { username, password } = this.state;
+    let payload = { username, password };
+
+    try {
+      let creator = await axios.post(
+        "http://localhost:11500/creators/",
+        payload
+      );
+console.log(creator)
+
+   
+
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  render() {
+    const { username, password } = this.state;
+    return (
+      <div className="container">
+        <h1>Welcome</h1>
+        <br />
+        <form>
+          <input
+            type="text"
+            placeholder="username"
+            onChange={this.handleInput}
+            name="username"
+            value={username}
+          ></input>
+          <input
+            type="text"
+            placeholder="password"
+            onChange={this.handleInput}
+            name="password"
+            value={password}
+          ></input>
+        </form>
+        <button onClick={this.submitForm}>Log In</button>
+        <Link to="/register">
+          <button onClick={this.handleSignUp}>Sign Up</button>
+        </Link>
+      </div>
+    );
+  }
 }
-
-export default Welcome
+export default Welcome;
