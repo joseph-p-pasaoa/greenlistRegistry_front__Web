061265692de1a66b9e88a6b9de9359140f7b1298
KathyPuma <kathypuma96@gmail.com>
2020-02-26 19:37:12 -0500

    drop down menu implemented for materials

--- src/Components/NavigationBar.jsx
+++ src/Components/NavigationBar.jsx
@@ -1,16 +1,61 @@
 import React from 'react'
+import axios from 'axios'
 import { Link } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import DropdownButton from 'react-bootstrap/DropdownButton'
+import Dropdown from 'react-bootstrap/Dropdown'
+import './NavigationBar.css'
 
-const NavigationBar = () => {
-  return (
-    <div class='navigationBar'>
-      <Link to='/main'>Main</Link>{' '}
-      <Link to='/creator/1'>Profile</Link>{' '}
-      <Link to='/addReclaimed'>Post reclaimed</Link>{' '}
-      <Link to='/material/1'>Material</Link>{' '}
-      <Link to='/'>Log out</Link>{' '}
-    </div>
-  )
+
+class NavigationBar extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      allMaterials: []
+    }
+  }
+
+  async componentDidMount() {
+    await this.handleGetMaterials()
+
+  }
+  async handleGetMaterials() {
+    const { allMaterials } = this.state
+    try {
+      let getMaterialInfo = await axios.get(`/materials`)
+      let getMaterialInfoData = getMaterialInfo.data.payload
+      this.setState({
+        allMaterials: getMaterialInfoData
+      })
+    } catch (err) {
+      console.log('ERROR', err)
+    }
+  }
+  render() {
+    const { allMaterials } = this.state
+    return (
+      <div className='container'>
+        <div className='navigationBar'>
+          <Link to='/main'>Main</Link>{' '}
+          <Link to={`/creator/${this.props.loggedUser.id}`}>Profile</Link>{' '}
+          <Link to='/addReclaimed'>Post reclaimed</Link>{' '}
+
+          <DropdownButton id="dropdown-basic-button" title="Material">
+            {allMaterials.map(material => {
+              return (
+                <Dropdown.Item href={`/material/${parseInt(material.id)}`}>{material.name}</Dropdown.Item>
+              )
+            })}
+
+          </DropdownButton>
+          <Link to='/'>Log out</Link>{' '}
+        </div>
+
+
+
+      </div>
+    )
+  }
 }
 
 export default NavigationBar
