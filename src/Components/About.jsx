afdebf9634ca3e4ae787cd2e7ff67a30bfff064b
KathyPuma <kathypuma96@gmail.com>
2020-02-29 13:23:52 -0500

    about page

---
+++ src/Components/About.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const About = () => {
+
+    return (
+        <div className='container'>
+            <h1>About</h1>
+            <h3>GreenList is an app that serves as a directory for new/small designers and help encourage/motivivate the use of sustainable materials to stop the continous cycle of fast fashion.
+                With this directory, designers can find companies who sell sustanible materials or find verdors who have any reclaimed materials. 
+                This app takes away the headache of trying to find suppliers by putting all of the information at their disposal. 
+                
+            </h3>
+        </div>
+    )
+
+}
+
+export default About
